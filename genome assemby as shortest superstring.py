a7e865f9334ffd31ce8d3b3fe1fe699db3b2bee4
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-07-17 15:34:30 -0400

    Update genome assemby as shortest superstring.py

--- genome assemby as shortest superstring.py
+++ genome assemby as shortest superstring.py
@@ -5,15 +5,12 @@ Created on Sun Jun 30 23:33:46 2024
 @author: gbulb
 """
 
-dict_strings={'Rosalind_56': 'ATTAGACCTG', 'Rosalind_57' : 'CCTGCCGGAA',  'Rosalind_58': 'AGACCTGCCG', 'Rosalind_59': 'GCCGGAATAC'}
-
-def find_min_len_of_keys(dict_strings):
-    list_of_lens=[]
-    for key in dict_strings.keys():
-        list_of_lens.append(len(dict_strings[key]))
-    return max(list_of_lens)
-max_len=find_min_len_of_keys(dict_strings)
 class substrings_to_superstring:
+    def find_min_len_of_keys(dict_strings):
+        list_of_lens=[]
+        for key in dict_strings.keys():
+            list_of_lens.append(len(dict_strings[key]))
+        return max(list_of_lens)
     def substrings_to_string(dict_strings):
         STRING=''
         for key in dict_strings.keys():
@@ -28,4 +25,7 @@ class substrings_to_superstring:
 
         
 if __name__ == "__main__":
+    dict_strings={'Rosalind_56': 'ATTAGACCTG', 'Rosalind_57' : 'CCTGCCGGAA',  'Rosalind_58': 'AGACCTGCCG', 'Rosalind_59': 'GCCGGAATAC'}
+    max_len=substrings_to_superstring.find_min_len_of_keys(dict_strings)
     substrings_to_superstring.substrings_to_string(dict_strings)
+               
